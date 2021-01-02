5fa494106fd7ccf9f87c7bcc5ed8016c93109208
Franck Rasolo <franckrasolo@users.noreply.github.com>
2021-01-02 18:32:16 +0000

    implementation of the pre-push hook

---
+++ src/pre_push/mod.rs
@@ -0,0 +1,92 @@
+use std::fmt::Write;
+use std::path::Path;
+
+use anyhow::{self, Context, Error, Result};
+use git2::{Delta, DiffDelta, DiffOptions, Repository, Diff};
+
+use crate::{Config, zero_oid};
+
+pub struct PrePush { repo: Repository, config: Config }
+
+#[derive(Debug)]
+pub struct Summary { pub contents: String }
+
+impl PrePush {
+    pub fn new(repo_path: &Path, config: Config) -> Self {
+        Self { repo: Repository::open(repo_path).expect("foo"), config }
+    }
+
+    pub fn run_hook(&self, input: &str) -> Result<Summary, Error> {
+        self.check_commits(&parse_range(input)?)
+    }
+
+    fn check_commits(&self, spec: &str) -> Result<Summary, Error> {
+        let range = self.repo.revparse(spec).with_context(|| format!("invalid range '{}'", spec))?;
+        let remote_commit = self.repo.find_commit(range.from().unwrap().id())?;
+        let local_commit  = self.repo.find_commit(range.to().unwrap().id())?;
+
+        let diff = self.repo.diff_tree_to_tree(
+            Some(&remote_commit.tree().unwrap()),
+            Some(&local_commit.tree().unwrap()),
+            Some(&mut DiffOptions::new())
+        ).unwrap();
+
+        self.config.check(diff)
+    }
+}
+
+fn parse_range(input: &str) -> Result<String, Error> {
+    let args = input.split_whitespace()
+        .filter_map(|arg| arg.parse().ok())
+        .take(4)
+        .collect::<Vec<String>>();
+
+    if let [_, local_oid, _, remote_oid] = &args[..] {
+        return Ok(create_range(local_oid, remote_oid))
+    }
+
+    anyhow::bail!("Expected: local_ref local_oid remote_ref remote_oid\n  Actual: '{}'", args.join(" "))
+}
+
+fn create_range(local_oid: &str, remote_oid: &str) -> String {
+    if local_oid == &zero_oid() {           // allow push / skip commits
+        String::new()
+    } else if remote_oid == &zero_oid() {   // new branch, examine all commits
+        local_oid.to_string()
+    } else {                                // examine all commits
+        format!("{}..{}", remote_oid, local_oid)
+    }
+}
+
+impl Config {
+    fn check(&self, diff: Diff) -> Result<Summary, Error> {
+        let mut output = String::new();
+
+        let count = diff.deltas()
+            .filter( |delta| self.countable(&delta))
+            .inspect(|delta| Config::append(&mut output, delta))
+            .count();
+
+        output.insert_str(0, &format!(">>> inspected changes: {} of {}", count, diff.deltas().count()));
+
+        if count <= self.change_threshold { return Ok(Summary { contents: output }) }
+        anyhow::bail!("This change set is too large: push fewer changes more frequently instead.\n{}", output)
+    }
+    
+    fn countable(&self, delta: &DiffDelta) -> bool {
+        vec![Delta::Added, Delta::Modified, Delta::Deleted].contains(&delta.status())
+            && self.ignored_patterns.iter().all(|p|
+                !p.matches(delta.new_file().path().unwrap().to_string_lossy().as_ref())
+            )
+    }
+
+    fn append(output: &mut String, delta: &DiffDelta) {
+        write!(output, "\n>>> {}\t{}",
+               format!("{:?}", delta.status()).chars().next().unwrap(),
+               delta.new_file().path().unwrap().to_string_lossy()
+        ).unwrap()
+    }
+}
+
+#[cfg(test)]
+mod unit_tests;
---
+++ src/pre_push/unit_tests.rs
@@ -0,0 +1,107 @@
+use std::path::Path;
+
+use anyhow::Error;
+use glob::Pattern;
+use spectral::*;
+use spectral::prelude::{ResultAssertions, StrAssertions};
+
+use crate::{Config, zero_oid};
+use crate::pre_push::{self, PrePush, Summary};
+
+#[test]
+fn create_range_when_local_branch_is_deleted() {
+    let remote_oid = "903f528740c6689a3213f167f2d22153e8b89cae";
+
+    let actual = pre_push::create_range(&zero_oid(), remote_oid);
+    assert_eq!(actual, "", "range: actual (left) vs expected (right)");
+}
+
+#[test]
+fn create_range_when_remote_branch_does_not_exist_yet() {
+    let local_oid = "4e5543acfcc15226d7e53409ce7891fe5bf8ca25";
+
+    let actual = pre_push::create_range(local_oid, &zero_oid());
+    assert_eq!(actual, local_oid, "range: actual (Left) vs expected (Right)");
+}
+
+#[test]
+fn create_range_when_revisions_are_not_zero() {
+    let local_oid  = "4e5543acfcc15226d7e53409ce7891fe5bf8ca25";
+    let remote_oid = "903f528740c6689a3213f167f2d22153e8b89cae";
+
+    let actual = pre_push::create_range(local_oid, remote_oid);
+    let expected = format!("{}..{}", remote_oid, local_oid);
+    assert_eq!(actual, expected, "range: actual (Left) vs expected (Right)");
+}
+
+#[test]
+fn parse_range_when_input_is_empty() {
+    let input = "";
+    let error = pre_push::parse_range(input).unwrap_err();
+    assert_eq!(format!("{}", error), parsing_error_for(input), "parsing error");
+}
+
+#[test]
+fn parse_range_when_input_lacks_some_values() {
+    let input = "a b";
+    let error = pre_push::parse_range(input).unwrap_err();
+    assert_eq!(format!("{}", error), parsing_error_for(input), "parsing error");
+}
+
+#[test]
+fn parse_range_when_input_has_all_expected_values() {
+    let spec = pre_push::parse_range("a b c d").unwrap();
+    assert_eq!(spec, "d..b", "parsed range");
+}
+
+#[test]
+fn parse_range_when_input_has_additional_values() {
+    let spec = pre_push::parse_range("a b c d e f").unwrap();
+    assert_eq!(spec, "d..b", "parsed range");
+}
+
+fn parsing_error_for(input: &str) -> String {
+    format!("Expected: local_ref local_oid remote_ref remote_oid\n  Actual: '{}'", input)
+}
+
+#[test]
+fn run_hook_when_local_revision_is_not_found() {
+    let local_oid  = "???";
+    let remote_oid = "764ecf6ed4de341203b9c9af94db4ac279c087fe";
+
+    let error_message = run_hook(local_oid, remote_oid).unwrap_err().to_string();
+
+    asserting!("push rejected")
+        .that(&error_message)
+        .is_equal_to(format!("invalid range '{}..{}'", remote_oid, local_oid));
+}
+
+#[test]
+fn run_hook_when_too_many_changes_are_pushed() {
+    let local_oid  = "764ecf6ed4de341203b9c9af94db4ac279c087fe";
+    let remote_oid = "c89ad800371ebf5300212ddd0523b14534fc99cc";
+
+    let error_message = run_hook(local_oid, remote_oid).unwrap_err().to_string();
+
+    asserting!("push rejected")
+        .that(&error_message)
+        .contains("change set is too large");
+}
+
+#[test]
+fn run_hook_when_few_changes_are_pushed() {
+    let local_oid  = "764ecf6ed4de341203b9c9af94db4ac279c087fe";
+    let remote_oid = "971595ab3f742abcf8c8cf839cff2a46a4d95feb";
+
+    assert_that!(run_hook(local_oid, remote_oid)).is_ok();
+}
+
+fn run_hook(local_oid: &str, remote_oid: &str) -> Result<Summary, Error> {
+    let repo_path = format!("{}/tests/resources/test-repo", env!("CARGO_MANIFEST_DIR"));
+    let config = Config {
+        ignored_patterns: vec![Pattern::new("*.yml").unwrap()],
+        change_threshold: 3
+    };
+    let input = format!("{} {} {} {}", "local_ref", local_oid, "remote_ref", remote_oid);
+    PrePush::new(Path::new(&repo_path), config).run_hook(&input)
+}
