214195758e9ac1479585b38f8f136bd05f1d0385
Franck Rasolo <franckrasolo@users.noreply.github.com>
2026-01-08 10:06:46 +0000

    use the latest Flake Checker from Nixpkgs instead

--- flake.nix
+++ flake.nix
@@ -53,7 +53,7 @@
             "
 
             # health checks for Nix flake inputs
-            nix run "github:DeterminateSystems/flake-checker/v0.2.8"
+            nix run https://flakehub.com/f/NixOS/nixpkgs/0.1#flake-checker
 
 #            rustup default stable
 #            rustup component add rust-src
